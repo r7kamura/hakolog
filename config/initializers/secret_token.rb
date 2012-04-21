3e8cb03bbd785a92d456d6e083d72a8df050e250
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 02:23:51 +0900

    Initial commit (rails new tamayura -j mysql -T)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tamayura::Application.config.secret_token = 'd229cf47cadc54b5b092a388aadc23536c30b5e5d910184cd45d4d5e76c4e4b3de54ee1cdb8aca85ec533d72c1f48a0dac8d8b03ec83e2eb95e872b175d251c8'
