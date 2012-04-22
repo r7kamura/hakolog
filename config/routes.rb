c687de8ad4aaf237aa0f8f3c913ed08a9f43613b
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 19:08:42 +0900

    add routes to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Tamayura::Application.routes.draw do
   root :to => "blogs#index"
 
   resources :blogs do
+    collection do
+      get "login"
+      get "login_callback"
+    end
+
     resources :entries
   end
 end
