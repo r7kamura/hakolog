ed023051c20687713bc23cb0055974c563a39613
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 11:43:24 +0900

    devide large method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # find blog by session
   def current_blog
-    @current_blog ||= begin
-      if session[:blog_id]
-        Blog.find_by_id(session[:blog_id])
-      elsif session[:dropbox]
-        Blog.find_by_dropbox_id(get_uid)
-      end
-    end
+    @current_blog ||= find_current_blog_by_session
   end
   helper_method :current_blog
 
   private
 
-  # find Dropbox user id by serialized session
-  def get_uid(dropbox_session)
+  def find_current_blog_by_session
+    if session[:blog_id]
+      Blog.find_by_id(session[:blog_id])
+    elsif session[:dropbox]
+      begin
+        uid = find_dropbox_uid_by_serialized_session(session[:dropbox])
+        Blog.find_by_dropbox_id(uid)
+      rescue DropboxAuthError
+        session.delete(:dropbox)
+        nil
+      end
+    end
+  end
+
+  def find_dropbox_uid_by_serialized_session(dropbox_session)
     ds     = DropboxSession.deserialize(dropbox_session)
     client = DropboxClient.new(ds, :app_folder)
     client.account_info["uid"]
