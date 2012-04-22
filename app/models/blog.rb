430bbbc512e40c5471552e25f0f90cf3edf5dfc6
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 21:39:46 +0900

    add more validations to Blog model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ActiveRecord::Base
   attr_accessible :username, :dropbox_id, :dropbox_session
 
-  validates :username, :uniqueness => true
-  validates :dropbox_session, :uniqueness => true
+  validates :username, :uniqueness => true, :presence => true
+  validates :dropbox_session, :uniqueness => true, :presence => true
+  validates :dropbox_id, :uniqueness => true, :presence => true
 end
