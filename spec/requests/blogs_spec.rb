24945f8912765ad15f2931c29ebfd3b2389d6608
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-23 02:31:10 +0900

    refactor spec

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe "Blogs" do
   shared_examples_for "an existing page" do
@@ -8,23 +8,32 @@ describe "Blogs" do
   subject { page }
 
   describe "GET /blogs" do
-    before do
-      visit blogs_path
-    end
-
+    before { visit blogs_path }
     it_should_behave_like "an existing page"
   end
 
   describe "GET /blogs/:id" do
-    before do
-      @blog = FactoryGirl.create(:blog)
-      visit blog_path(:id => @blog.id)
+    before { visit url_for(blog) }
+
+    describe "status code" do
+      let(:blog) { FactoryGirl.create(:blog) }
+      it_should_behave_like "an existing page"
     end
 
-    it_should_behave_like "an existing page"
+    describe "title" do
+      shared_examples_for "it has title" do
+        it { should have_content(blog.title) }
+      end
 
-    it do
-      should have_content(@blog.title)
+      context "when blog has its title" do
+        let(:blog) { FactoryGirl.create(:blog, :title => "blog title") }
+        it_should_behave_like("it has title")
+      end
+
+      context "when blog does not have its title" do
+        let(:blog) { FactoryGirl.create(:blog) }
+        it_should_behave_like("it has title")
+      end
     end
   end
 end
