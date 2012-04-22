5c9947b04d5a1fdea79ea3c34aba8222d51b55d2
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 19:58:14 +0900

    add login action into blogs controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,4 +4,17 @@ class BlogsController < ApplicationController
 
   def show
   end
+
+  def login
+    ds = DropboxSession.new(Setting.consumer_key, Setting.consumer_secret)
+    session[:dropbox] = ds.serialize
+    redirect_to ds.get_authorize_url(login_callback_blogs_path)
+  end
+
+  def login_callback
+    ds = DropboxSession.deserialize(session[:dropbox])
+    ds.get_access_token
+    session[:dropbox] = ds.serialize
+    redirect_to :blogs
+  end
 end
