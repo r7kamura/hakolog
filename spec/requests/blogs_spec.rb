9a90b5e66c97420a953c11c88967a3f8e2052404
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-23 02:12:17 +0900

    change spec to use visit

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -3,22 +3,23 @@ require 'spec_helper'
 describe "Blogs" do
   shared_examples_for "an existing page" do
     it "should return status code of 200" do
-      response.status.should be(200)
+      page.status_code.should be(200)
     end
   end
 
   describe "GET /blogs" do
     before do
-      get blogs_path
+      visit blogs_path
     end
     it_should_behave_like "an existing page"
   end
 
   describe "GET /blogs/:id" do
     before do
-      blog = FactoryGirl.create(:blog)
-      get blog_path(:id => blog.id)
+      @blog = FactoryGirl.create(:blog)
+      visit blog_path(:id => @blog.id)
     end
+
     it_should_behave_like "an existing page"
   end
 end
