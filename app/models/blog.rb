a12ab5270d5543310b91d952aa2bd0135c95ee81
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-22 21:35:09 +0900

    should use attr_accessible

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ActiveRecord::Base
+  attr_accessible :username, :dropbox_id, :dropbox_session
+
+  validates :username, :uniqueness => true
+  validates :dropbox_session, :uniqueness => true
 end
