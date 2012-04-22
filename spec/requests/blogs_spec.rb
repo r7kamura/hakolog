9cd8658cfad02879c6d24b55476e5372f667d681
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-23 02:00:23 +0900

    add spec to request /blogs/:id

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe "Blogs" do
-  describe "GET /blogs" do
-    it do
-      get blogs_path
+  shared_examples_for "status code 200" do
+    it "should return status code of 200" do
       response.status.should be(200)
     end
+  end
+
+  describe "GET /blogs" do
+    before do
+      get blogs_path
+    end
+    it_should_behave_like "status code 200"
+  end
+
+  describe "GET /blogs/:id" do
+    before do
+      blog = FactoryGirl.create(:blog)
+      get blog_path(:id => blog.id)
+    end
+    it_should_behave_like "status code 200"
   end
 end
