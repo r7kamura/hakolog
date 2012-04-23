482cf4aa53f5dfae6873fb738ccf04ee99b61afd
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-23 20:43:37 +0900

    define blog has_many entries association

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 class Blog < ActiveRecord::Base
   attr_accessible :username, :dropbox_id, :dropbox_session
 
+  has_many :entries
+
   validates :username, :uniqueness => true, :presence => true
   validates :dropbox_session, :uniqueness => true, :presence => true
   validates :dropbox_id, :uniqueness => true, :presence => true
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
   attr_accessible :blog_id, :body, :extension, :title
+
+  belongs_to :blog
 end
