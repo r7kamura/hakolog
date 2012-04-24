e6bc073d80b7cba7657038ef6199941ccf1c0c02
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-24 20:59:16 +0900

    refactor: change quotation type

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
-require 'spork'
+require "spork"
 
 Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
+  ENV["RAILS_ENV"] ||= "test"
   require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun'
+  require "rspec/rails"
+  require "rspec/autorun"
 
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
