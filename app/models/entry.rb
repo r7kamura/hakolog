7c107e60b515986837ad70370feb15411d8d7856
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-28 15:15:08 +0900

    change entry.body to parse it by markdown

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,16 @@ class Entry < ActiveRecord::Base
   belongs_to :blog
 
   BASE_PATH = "/entries/"
+  MARKDOWN_OPTION = {
+    :autolink            => true,
+    :fenced_code_blocks  => true,
+    :lax_html_blocks     => true,
+    :strikethrough       => true,
+    :generate_toc        => true,
+    :hard_wrap           => true,
+    :tables              => true,
+    :no_intra_emphasis   => true,
+  }
 
   def self.create_with_path(args)
     create args.merge(:path => BASE_PATH + args.delete(:path))
@@ -11,6 +21,11 @@ class Entry < ActiveRecord::Base
 
   def title
     path && path.split(BASE_PATH, 2).last
+  end
+
+  def body
+    parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML, MARKDOWN_OPTION)
+    parser.render(super).html_safe
   end
 
   def can_overwrite?(modified_at)
