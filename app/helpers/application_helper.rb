fa88fadeceb83a1311fdda507ab6a02c2df1dbec
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-28 15:35:17 +0900

    add unicode helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  UNICODE_MAP = {
+    :home   => 8682,
+    :edit   => 9997,
+    :logout => 8998,
+  }
+
+  def unicode(name)
+    "&##{UNICODE_MAP[name]};".html_safe
+  end
 end
