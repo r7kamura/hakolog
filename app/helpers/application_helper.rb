960a6a7bc1d589542f6f507804d3a8898ede8503
Ryo NAKAMURA <r7kamura@gmail.com>
2012-04-28 21:08:56 +0900

    add design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,14 @@ module ApplicationHelper
     :home   => 8682,
     :edit   => 9997,
     :logout => 8998,
+    :hot    => 9832,
   }
 
   def unicode(name)
     "&##{UNICODE_MAP[name]};".html_safe
+  end
+
+  def unique_class_name
+    controller.controller_name + "_" + controller.action_name
   end
 end
