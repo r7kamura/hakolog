b0bd1ab3eb53e321da6241633de355fe1fa8a5f7
Ryo NAKAMURA <r7kamura@gmail.com>
2012-05-02 23:32:04 +0900

    fix: except(:order) does not have effect?

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,7 +14,7 @@ class Entry < ActiveRecord::Base
       columns = column_names
       columns.delete("modified_at")
       columns.push("DATE(modified_at)")
-      group(columns).except(:order)
+      group(columns).reorder(:id)
     end
   }
 
